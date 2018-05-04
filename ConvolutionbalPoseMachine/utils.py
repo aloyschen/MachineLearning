978ebb0a2f8030f1d7aea24ee98990c009d501b2
gaochen3 <gaochen3@staff.weibo.com>
2018-05-04 10:40:53 +0800

    add 姿态估计

---
+++ ConvolutionbalPoseMachine/utils.py
@@ -0,0 +1,73 @@
+import json
+from scipy.io import loadmat
+
+def save_joints():
+    joint_data_fn = './dataset/mpii/data.json'
+    mat = loadmat('./dataset/mpii/mpii_human_pose_v1_u12_1.mat')
+
+    fp = open(joint_data_fn, 'w')
+
+    for i, (anno, train_flag) in enumerate(
+        zip(mat['RELEASE']['annolist'][0, 0][0],
+            mat['RELEASE']['img_train'][0, 0][0])):
+
+        img_fn = anno['image']['name'][0, 0][0]
+        train_flag = int(train_flag)
+
+        head_rect = []
+        if 'x1' in str(anno['annorect'].dtype):
+            head_rect = zip(
+                [x1[0, 0] for x1 in anno['annorect']['x1'][0]],
+                [y1[0, 0] for y1 in anno['annorect']['y1'][0]],
+                [x2[0, 0] for x2 in anno['annorect']['x2'][0]],
+                [y2[0, 0] for y2 in anno['annorect']['y2'][0]])
+
+        if 'annopoints' in str(anno['annorect'].dtype):
+            annopoints = anno['annorect']['annopoints'][0]
+            head_x1s = anno['annorect']['x1'][0]
+            head_y1s = anno['annorect']['y1'][0]
+            head_x2s = anno['annorect']['x2'][0]
+            head_y2s = anno['annorect']['y2'][0]
+            for annopoint, head_x1, head_y1, head_x2, head_y2 in zip(
+                    annopoints, head_x1s, head_y1s, head_x2s, head_y2s):
+                if annopoint != []:
+                    head_rect = [float(head_x1[0, 0]),
+                                 float(head_y1[0, 0]),
+                                 float(head_x2[0, 0]),
+                                 float(head_y2[0, 0])]
+
+                    # joint coordinates
+                    annopoint = annopoint['point'][0, 0]
+                    j_id = [str(j_i[0, 0]) for j_i in annopoint['id'][0]]
+                    x = [x[0, 0] for x in annopoint['x'][0]]
+                    y = [y[0, 0] for y in annopoint['y'][0]]
+                    joint_pos = {}
+                    for _j_id, (_x, _y) in zip(j_id, zip(x, y)):
+                        joint_pos[str(_j_id)] = [float(_x), float(_y)]
+                    # joint_pos = fix_wrong_joints(joint_pos)
+
+                    # visiblity list
+                    if 'is_visible' in str(annopoint.dtype):
+                        vis = [v[0] if v else [0]
+                               for v in annopoint['is_visible'][0]]
+                        vis = dict([(k, int(v[0])) if len(v) > 0 else v
+                                    for k, v in zip(j_id, vis)])
+                    else:
+                        vis = None
+
+                    if len(joint_pos) == 16:
+                        data = {
+                            'filename': img_fn,
+                            'train': train_flag,
+                            'head_rect': head_rect,
+                            'is_visible': vis,
+                            'joint_pos': joint_pos
+                        }
+
+                        print(json.dumps(data), file=fp)
+import numpy as np
+arr3D = np.array([[[255], [255]],
+                  [[255], [255]]])
+print(arr3D.shape)
+pad = np.pad(arr3D, ((0, 0), (1, 1), (0, 0)), 'constant')
+print('constant:  \n' + str(pad.shape))
